9a4df56c867cbcd6f5b997ae5c3631857b4286eb
andrey demidov <demas@fastmail.com>
2018-01-06 14:12:43 +0300

    Перенес правила конфигуратора во внешний json файл

--- pkg/services/classificators/stack_classificator.go
+++ pkg/services/classificators/stack_classificator.go
@@ -1,12 +1,53 @@
 package classificators
 
 import (
-	"github.com/demas/observer/pkg/services"
 	"github.com/demas/observer/pkg/models"
+	"github.com/demas/observer/pkg/services"
+	"os"
+	"io/ioutil"
+	"encoding/json"
 )
 
+type FirstLevelRule struct {
+	Site string `json:"site"`
+	Include string `json:"include"`
+	Result string `json:"result"`
+}
 
-var log = services.GetLogger("stack_classificator")
+type SecondLevelRule struct {
+	Site string  `json:"site"`
+	First string  `json:"first"`
+	Include string  `json:"include"`
+	Result string `json:"result"`
+}
+
+type StackClassificatorRules struct {
+	StopTags []string `json:"stop_tags"`
+	FirstLevelRules []FirstLevelRule `json:"first_level_rules"`
+	SecondLevelRules []SecondLevelRule `json:"second_level_rules"`
+}
+
+const rulesFileName = "./classificator.json"
+
+var rules StackClassificatorRules
+var log = services.GetLogger("classificator")
+
+func init() {
+
+	jsonFile, err := os.Open(rulesFileName)
+	if err != nil {
+		log.Error(err.Error())
+		panic(err)
+	}
+	defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	err = json.Unmarshal(byteValue, &rules)
+	if err != nil {
+		log.Error(err.Error())
+		panic(err)
+	}
+}
 
 // Check tag existence in the question
 func containTag(q models.SOQuestion, tag string) (bool) {
@@ -16,29 +57,24 @@ func containTag(q models.SOQuestion, tag string) (bool) {
 			return true
 		}
 	}
-
 	return false
 }
-
 
 // Check stop tag existance in the question
 func containStopTag(q models.SOQuestion) (bool, string) {
 
-	for _, stop_tag := range stop_tags {
+	for _, stop_tag := range rules.StopTags {
 		if containTag(q, stop_tag) {
 			return true, stop_tag
 		}
 	}
-
 	return false, ""
 }
 
 func firstLevelClassification(q models.SOQuestion, site string) (string) {
 
-	for _, flr := range firstLevelRules {
-
+	for _, flr := range rules.FirstLevelRules {
 		if site == flr.Site {
-
 			if flr.Include == "*" {
 				return flr.Result
 			} else if containTag(q, flr.Include) {
@@ -46,19 +82,17 @@ func firstLevelClassification(q models.SOQuestion, site string) (string) {
 			}
 		}
 	}
-
-	return ""
+	return "general"
 }
 
 func secondLevelClassification(q models.SOQuestion, site string, first string) (string) {
 
-	for _, slr := range secondLevelRules {
+	for _, slr := range rules.SecondLevelRules {
 
 		if (slr.Site == site)  && (slr.First == first) && containTag(q, slr.Include) {
 			return slr.Result
 		}
 	}
-
 	return "general"
 }
 
